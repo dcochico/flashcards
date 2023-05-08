e2d19e83d8ef8bd3720bff860e03f8356b1435ff
Daniel Cochico <dcochico.20@gmail.com>
2023-05-07 18:17:46 -0700

    Make game playable in console

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-// This is where your project starts.
+const { prototypeData } = require('./src/data');
+const { createCard } = require('./src/card');
+const { createDeck } = require('./src/deck');
+const { createRound } = require('./src/round');
+const { printMessage, printQuestion } = require('./src/game');
+console.log('Your project is running...');
 
-console.log('Your project is running...'); 
+const start = myDeck => {
+  const cards = myDeck.map(card => createCard(card.id, card.question, card.answers, card.correctAnswer));
+  const deck = createDeck(cards);
+  const round = createRound(deck);
+  printMessage(deck);
+  printQuestion(round);
+};
+
+start(prototypeData);
