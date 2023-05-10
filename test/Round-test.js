2a337cd4368061b80d02e33ae9059630f95b7ba1
Daniel Cochico <dcochico.20@gmail.com>
2023-05-10 16:22:33 -0700

    Use arrow functions for round tests

--- test/Round-test.js
+++ test/Round-test.js
@@ -1,15 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const { createCard } = require('../src/card');
 const { createDeck } = require('../src/deck');
 const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 const { prototypeData } = require('../src/data');
 
-describe('', function() {
+describe('', () => {
   let card1, card2, card3, deck1, deck2, round1, round2;
 
-  beforeEach(function() {
+  beforeEach(() => {
     card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     card2 = createCard(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     card3 = createCard(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
@@ -19,122 +18,76 @@ describe('', function() {
     round2 = createRound(deck2);
   });
 
-  describe('createRound', function() {
-    it('should be a function', function() {
-      expect(createRound).to.be.a('function');
-    });
-
-    it('should hold a deck of cards', function() {
+  describe('createRound', () => {
+    it('should be a function', () => expect(createRound).to.be.a('function'));
+    it('should hold a deck of cards', () => {
       expect(round1.deck).to.deep.equal(deck1);
       expect(round2.deck).to.deep.equal(deck2);
     });
-
-    it('should start at the first card in the deck', function() {
+    it('should start at the first card in the deck', () => {
       expect(round1.currentCard).to.deep.equal(card1);
       expect(round2.currentCard).to.deep.equal(prototypeData[0]);
     });
-
-    it('should start with 0 turns', function() {
-      expect(round2.turns).to.deep.equal(0);
-    });
-
-    it('should start with an empty array of incorrectly guessed cards', function() {
-      expect(round2.incorrectGuesses).to.deep.equal([]);
-    });
+    it('should start with 0 turns', () => expect(round2.turns).to.deep.equal(0));
+    it('should start with an empty array of incorrectly guessed cards', () => expect(round2.incorrectGuesses).to.deep.equal([]));
   });
 
-  describe('takeTurn', function() {
-    it('should be a function', function() {
-      expect(takeTurn).to.be.a('function');
-    });
-
-    it('should update the turns count', function () {
+  describe('takeTurn', () => {
+    it('should be a function', () => expect(takeTurn).to.be.a('function'));
+    it('should update the turns count', () => {
       expect(round1.turns).to.deep.equal(0);
-
       takeTurn('sea otter', round1);
-
       expect(round1.turns).to.deep.equal(1);
-
       takeTurn('gallbladder', round1);
-
       expect(round1.turns).to.deep.equal(2);
     });
 
-    it('should replace current card with next card', function() {
+    it('should replace current card with next card', () => {
       expect(round1.currentCard).to.deep.equal(card1);
-
       takeTurn('sea otter', round1);
-
       expect(round1.currentCard).to.deep.equal(card2);
-
       takeTurn('gallbladder', round1);
-
       expect(round1.currentCard).to.deep.equal(card3);
     });
 
-    it('should store each incorrectly guessed card', function() {
+    it('should store each incorrectly guessed card', () => {
       expect(round1.incorrectGuesses).to.deep.equal([]);
-
       takeTurn('pug', round1);
-
       expect(round1.incorrectGuesses).to.deep.equal([card1]);
-
       takeTurn('spleen', round1);
-
       expect(round1.incorrectGuesses).to.deep.equal([card1, card2]);
-
       takeTurn('Fitzgerald', round1);
-
       expect(round1.incorrectGuesses).to.deep.equal([card1, card2]);
     });
 
-    it('should return feedback whether guess is correct or not', function() {
+    it('should return feedback whether guess is correct or not', () => {
       const turn1 = takeTurn('pug', round1);
-
       expect(turn1).to.deep.equal('incorrect!')
-
       const turn2 = takeTurn('gallbladder', round1);
-
       expect(turn2).to.deep.equal('correct!');
     });
   });
 
-  describe('calculatePercentCorrect', function() {
-    it('should be a function', function() {
-      expect(calculatePercentCorrect).to.be.a('function');
-    });
-
-    it('should calculate and return the percentage of correct guesses', function() {
+  describe('calculatePercentCorrect', () => {
+    it('should be a function', () => expect(calculatePercentCorrect).to.be.a('function'));
+    it('should calculate and return the percentage of correct guesses', () => {
       takeTurn('sea otter', round1);
-
       const percentCorrect1 = calculatePercentCorrect(round1);
-
       expect(percentCorrect1).to.deep.equal(100);
-
       takeTurn('spleen', round1);
-
       const percentCorrect2 = calculatePercentCorrect(round1);
-
       expect(percentCorrect2).to.deep.equal(50);
     });
   });
 
-  describe('endRound', function() {
-    it('should be a function', function() {
-      expect(endRound).to.be.a('function');
-    });
-
-    it('should end round and print correct guess percentage', function() {
+  describe('endRound', () => {
+    it('should be a function', () => expect(endRound).to.be.a('function'));
+    it('should end round and print correct guess percentage', () => {
       takeTurn('sea otter', round1);
-
       const end1 = endRound(round1);
-
       expect(end1).to.deep.equal(`** Round over! ** You answered ${100}% of the questions correctly!`);
-
       takeTurn('array', round2);
-
       const end2 = endRound(round2);
-
       expect(end2).to.deep.equal(`** Round over! ** You answered ${0}% of the questions correctly!`);
     });
   });
