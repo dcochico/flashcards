41d78f51081010968da6c5e29efa6b04d4270b0f
Daniel Cochico <dcochico.20@gmail.com>
2023-05-10 16:13:29 -0700

    Convert to arrow functions

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,16 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const { createCard } = require('../src/card');
 
-describe('createCard', function() {
-  it('should be a function', function() {
-    expect(createCard).to.be.a('function');
-  });
-
-  it('should create a card and its properties', function() {
+describe('createCard', () => {
+  it('should be a function', () => expect(createCard).to.be.a('function'));
+  it('should create a card and its properties', () => {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    
     expect(card.id).to.equal(1);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,19 +1,14 @@
 const chai = require('chai');
 const expect = chai.expect;
-
 const { createCard } = require('../src/card');
 const { evaluateGuess } = require('../src/turn');
 
-describe('evaluateGuess', function() {
-  it('should be a function', function() {
-    expect(evaluateGuess).to.be.a('function');
-  });
-
-  it('should evaluate if a guess is correct or incorrect', function() {
+describe('evaluateGuess', () => {
+  it('should be a function', () => expect(evaluateGuess).to.be.a('function'));
+  it('should evaluate if a guess is correct or incorrect', () => {
     const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const correctGuess = evaluateGuess('object', card);
     const incorrectGuess = evaluateGuess('array', card);
-
     expect(correctGuess).to.deep.equal('correct!');
     expect(incorrectGuess).to.deep.equal('incorrect!');
   });
